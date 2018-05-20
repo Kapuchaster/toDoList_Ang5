2e0210e6c6658730035f2f5e8195401f13a51935
Michal Kapuscinski <kapuchaster@gmail.com>
2018-05-20 11:49:33 +0200

    init angular 5 app + add Bootstrap 4

---
+++ UI/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToDoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
+  ],
+  providers: [],
+  exports: [BsDropdownModule, TooltipModule, ModalModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
